be109a29162eca7a84ee065e05a530259cb38b21
Daniel Brodsky <d6brodsky@gmail.com>
2018-10-25 18:18:10 -0700

    added more RPC calls

--- go/src/blockchain/miner/miner.go
+++ go/src/blockchain/miner/miner.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"blockchain/miner/minerlib"
 	"encoding/gob"
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/rpc"
@@ -10,7 +12,10 @@ import (
 )
 
 type M interface {
-	MakeKnown(addr string) error
+	MakeKnown(addr string, reply *int) error
+	ReceiveOp(op string, reply *int) error
+	ReceiveBlock(block minerlib.Block, reply *int) error
+
 }
 
 type Miner struct {
@@ -32,6 +37,12 @@ type Miner struct {
 
 var (
 	minerConnections = make(map[string]*rpc.Client)
+	waitingOps = make(map[string]bool)
+	blocks = make(map[string]*minerlib.Block)
+
+	currentLongest *minerlib.Block
+	genesisBlock *minerlib.Block
+
 )
 
 
@@ -44,10 +55,76 @@ func (miner Miner) MakeKnown(addr string, reply *int) error {
 			log.Println("dialing:", err)
 		}
 	}
-	*reply = 1
 	return nil
 }
 
+func (miner Miner) ReceiveOp(op string, reply *int) error {
+	if _, ok := waitingOps[op]; !ok && minerlib.ValidateOp() {
+		// op not received yet, store and flood it
+		waitingOps[op] = true
+		for _, conn := range minerConnections {
+			conn.Go("Miner.ReceiveOp", op, nil, nil)
+		}
+
+		// TODO: Check timeout and maxOps and see if it's time to mine a new block
+	}
+	return nil
+}
+
+func (miner Miner) ReceiveBlock(returnAddr string, hash string, block minerlib.Block, reply *int) error {
+	// TODO: Implement ValidateBlock
+
+	// if miner is behind, get previous blocks until caught up
+	if _, ok := blocks[block.PrevHash]; !ok {
+
+		// add missing blocks to a temp store in case they're fake
+		missingBlocks := make(map[string]*minerlib.Block)
+		missingBlocks[hash] = &block
+
+		for !ok {
+			var prevBlock *minerlib.Block
+			minerConnections[returnAddr].Call("Miner.SendPreviousBlock", block.PrevHash, &prevBlock)
+			missingBlocks[block.PrevHash] = prevBlock
+			block = *prevBlock
+			_, ok = blocks[block.PrevHash]
+		}
+
+	}
+
+	// TODO: change to blockmap insert
+	if minerlib.ValidateBlock() {
+		addToTree(block, hash)
+
+		// send the block to connected miners
+		for _, conn := range minerConnections {
+			conn.Go("Miner.ReceiveBlock", block, nil, nil)
+		}
+	}
+	return nil
+}
+
+func (miner Miner) SendPreviousBlock(hash string, block *minerlib.Block) error {
+
+	*block = *blocks[hash]
+
+	return nil
+}
+
+func addToTree(block minerlib.Block, hash string) {
+	blocks[hash] = &block
+	updateLongest(block)
+}
+
+func updateLongest(block minerlib.Block) {
+	if block.Depth == currentLongest.Depth {
+		if rand.Intn(2) == 1 {
+			currentLongest = &block
+		}
+	}
+	if block.Depth > currentLongest.Depth {
+		currentLongest = &block
+	}
+}
 
 func main() {
 
@@ -61,6 +138,7 @@ func main() {
 	} else { log.Fatal("file error:", e)}
 	file.Close()
 
+	// TODO: add genesis block
 
 	// Open RPC server for other miners
 	rpc.Register(miner)
