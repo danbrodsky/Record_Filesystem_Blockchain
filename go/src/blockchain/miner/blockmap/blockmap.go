18bf9744f0e3adb887f0ce1919f927e81b068983
q1v0b <q1v0b@example.com>
2018-10-26 14:51:49 -0700

    mining done

--- go/src/blockchain/miner/blockmap/blockmap.go
+++ go/src/blockchain/miner/blockmap/blockmap.go
@@ -7,8 +7,8 @@ import (
 )
 
 type BlockMap struct {
-    TailBlock Block
-    GenesisBlock Block
+    tailBlock Block
+    genesisBlock Block
     Map map[string]Block
 }
 
@@ -28,11 +28,11 @@ type Block struct{
 }
 
 func NewBlockMap(genesisBlock Block) (blockmap BlockMap) {
-	// TODO check if block is genesis
+    // TODO check if block is genesis
     blockmap = BlockMap{}
     genesisBlock.Depth =  0
-    blockmap.TailBlock = genesisBlock
-    blockmap.GenesisBlock = genesisBlock
+    blockmap.tailBlock = genesisBlock
+    blockmap.genesisBlock = genesisBlock
     blockmap.Map = make(map[string]Block)
     blockmap.Map[getHash(genesisBlock)] = genesisBlock
     return blockmap
@@ -45,20 +45,31 @@ func (e PrevHashDoesNotExistError) Error() string {
     return fmt.Sprintf("Block-Map: Error hash does not exist in map [%s]", string(e))
 }
 
+type BlockNotValidError string
+
+func (e BlockNotValidError) Error() string {
+    return fmt.Sprintf("Block-Map: Error block does not end with continuous 0s [%s]", string(e))
+}
+
+// Gets the hash of the block
 func getHash(block Block) string{
      h := md5.New()
      h.Write([]byte(fmt.Sprintf("%v", block)))
      return hex.EncodeToString(h.Sum(nil))
 }
 
+// Inserts a block in the block map
+// Precondition Block should be valid that is all fields should be filled
+// and the the previous hash should exist
+// also the hash of the block should end with some number of 0s
 func (bm *BlockMap) Insert(block Block) (err error){
-	// TODO verify block hash end of 0s
-    if _, ok := bm.Map[block.PrevHash]; !ok {
-	block.Depth = bm.TailBlock.Depth + 1
-	fmt.Println("block to add:", bm.TailBlock)
+    if(!BHashEndsWithZeros(block,4)){ // TODO set env variable
+	return BlockNotValidError(getHash(block))
+    }
+    if _, ok := bm.Map[block.PrevHash]; ok {
         bm.Map[getHash(block)] = block
-	bm.TailBlock = block
-	fmt.Println("tail:", bm.TailBlock)
+	bm.tailBlock = block
+	fmt.Println("tail:", bm.tailBlock)
 	return nil
     } else {
 	return PrevHashDoesNotExistError(block.PrevHash)
@@ -66,18 +77,54 @@ func (bm *BlockMap) Insert(block Block) (err error){
 }
 
 func (bm *BlockMap) GetMap() (map[string]Block){
-	return bm.Map
+    return bm.Map
 }
 
+func BHashEndsWithZeros(block Block, numZeros int) bool{
+    hash := getHash(block)
+    for i:= len(hash) - 1; i > len(hash)-1 -numZeros ; i--{
+        if(hash[i] != '0'){
+	    return false
+	}
+    }
+    return true
+}
+
+func (bm *BlockMap) SetTailBlock(block Block){
+    bm.tailBlock = block
+}
+
+// Mines a block and puts it in the block chain
+// ops is the operation 
+// minerId is the miner of the miner
+func (bm *BlockMap) MineAndAddBlock(ops []string, minerId string, blockCh chan *Block){
+    block := Block{ PrevHash: getHash(bm.tailBlock),
+		    Ops:ops,
+		    MinerId:minerId,
+		    Depth: bm.tailBlock.Depth+1 }
+    minedBlock := ComputeBlock(block , 4) // TODO set numZeros
+    if(minedBlock != nil){
+        bm.Insert(*minedBlock)
+	blockCh <-minedBlock
+    } else{
+	blockCh <-nil
+    }
+}
+
+
+// gets the longest chain of the blockchain with the first array to be the most recent
+// block in the map and the last to be the genesis block
 func (bm *BlockMap) GetLongestChain() ([]Block){
     var blockChain []Block
-    var currBlock = bm.TailBlock
-    for bm.Map[currBlock.PrevHash].PrevHash == bm.GenesisBlock.PrevHash {
-        blockChain = append(blockChain, bm.Map[currBlock.PrevHash])
+    var currBlock = bm.tailBlock
+    for currBlock.PrevHash != bm.genesisBlock.PrevHash {
+        blockChain = append(blockChain, currBlock)
+	currBlock = bm.Map[currBlock.PrevHash]
     }
-    blockChain = append(blockChain, bm.GenesisBlock)
+    blockChain = append(blockChain, bm.genesisBlock)
     return blockChain
 }
+
 
 
 
--- go/src/blockchain/miner/blockmap/blockmap_test.go
+++ go/src/blockchain/miner/blockmap/blockmap_test.go
@@ -6,14 +6,46 @@ import(
 )
 
 func TestInsert(t *testing.T) {
+    blockCh := make(chan *Block,5)
     genesisBlock := Block{ PrevHash: "GENESIS", Nonce:"1" , MinerId:"james"}
     bm := NewBlockMap(genesisBlock)
-    block1 := Block{ PrevHash: "1234", Nonce:"1" , MinerId:"james"}
-    block2 := Block{ PrevHash: "1235", Nonce:"2" , MinerId:"james"}
-    block3 := Block{ PrevHash: "126", Nonce:"3" , MinerId:"james"}
-    bm.Insert(block1)
-    bm.Insert(block2)
-    bm.Insert(block3)
+    PrepareMining()
+    go bm.MineAndAddBlock(nil,"james",blockCh)
+    <-blockCh
+    go bm.MineAndAddBlock(nil,"james",blockCh)
+    <-blockCh
+    go bm.MineAndAddBlock(nil,"james",blockCh)
+    <-blockCh
+    go bm.MineAndAddBlock(nil,"james",blockCh)
+    <-blockCh
+    go bm.MineAndAddBlock(nil,"james",blockCh)
+    <-blockCh
+    fmt.Println("block map",bm.GetMap())
+    fmt.Println("longest chain",bm.GetLongestChain())
+    fmt.Println("block end with 3 zeros:",  BHashEndsWithZeros(bm.GetLongestChain()[1],3))
+    fmt.Println("block end with 4 zeros:",  BHashEndsWithZeros(bm.GetLongestChain()[1],4))
+    fmt.Println("block end with 5 zeros:",  BHashEndsWithZeros(bm.GetLongestChain()[1],5))
+    fmt.Println("block end with 6 zeros:",  BHashEndsWithZeros(bm.GetLongestChain()[1],6))
+}
+
+
+func TestStop(t *testing.T) {
+    blockCh := make(chan *Block,5)
+    genesisBlock := Block{ PrevHash: "GENESIS", Nonce:"1" , MinerId:"james"}
+    bm := NewBlockMap(genesisBlock)
+    PrepareMining()
+    go bm.MineAndAddBlock(nil,"james",blockCh)
+    StopMining()
+    <-blockCh
+    PrepareMining()
+    go bm.MineAndAddBlock(nil,"james",blockCh)
+    StopMining()
+    <-blockCh
+    PrepareMining()
+    go bm.MineAndAddBlock(nil,"james",blockCh)
+    StopMining()
+    <-blockCh
     fmt.Println("block map",bm.GetMap())
     fmt.Println("longest chain",bm.GetLongestChain())
 }
+
